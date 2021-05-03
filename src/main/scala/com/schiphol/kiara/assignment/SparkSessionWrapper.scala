51bd99c803f8a163701350f6165705929993ae84
Kiara Grouwstra <kiara@Dataworkz.nl>
2021-05-03 17:13:48 +0200

    log level warn

--- src/main/scala/com/schiphol/kiara/assignment/SparkSessionWrapper.scala
+++ src/main/scala/com/schiphol/kiara/assignment/SparkSessionWrapper.scala
@@ -5,12 +5,14 @@ import org.apache.spark.sql.SparkSession
 trait SparkSessionWrapper extends Serializable {
 
   lazy val spark: SparkSession = {
-    SparkSession
+    val session = SparkSession
       .builder()
       .master("local")
       .appName("spark session")
       .config("spark.sql.streaming.forceDeleteTempCheckpointLocation", true)
       .getOrCreate()
+    session.sparkContext.setLogLevel("WARN")
+    session
   }
 
 }
